4e9b55ab36747b517be24e7c6ef11f7bef5e991c
Kamil Stukus <kamistukus.kontakt@gmail.com>
2020-09-05 19:30:47 +0200

    Part II Update albumTemplate

---
+++ src/components/AlbumWrapper/AlbumWrapper.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const AlbumWrapper = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  margin: 50px;
+  grid-column-gap: 50px;
+`;
+
+export default AlbumWrapper;
--- src/templates/albumTemplate.js
+++ src/templates/albumTemplate.js
@@ -1,13 +1,21 @@
 import React from 'react';
-import Layout from '../layout/Layout';
 import { Link } from 'gatsby';
+import Layout from '../layout/Layout';
+import AlbumWrapper from '../components/AlbumWrapper/AlbumWrapper';
 
 const AlbumTemplate = ({ pageContext: { data } }) => (
   <Layout>
     <Link to="/">Go back</Link>
-    <h1>{data.title}</h1>
-    <h2>{data.artist.name}</h2>
-    <p>{data.description}</p>
+    <AlbumWrapper>
+      <div>
+        <img src={data.thumbnail} alt="Album cover" />
+      </div>
+      <div>
+        <h1>{data.title}</h1>
+        <h2>{data.artist.name}</h2>
+        <p>{data.description}</p>
+      </div>
+    </AlbumWrapper>
   </Layout>
 );
 
